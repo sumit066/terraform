a3e1814145c560be45ff62b4d237c0e801908c4a
sumit066 <84434082+sumit066@users.noreply.github.com>
2025-06-29 20:09:45 +0100

    Update providers.tf

--- modules-call/providers.tf
+++ modules-call/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.115"
+      version = "~>3.0"
     }
     azuredevops = {
       source  = "microsoft/azuredevops"
